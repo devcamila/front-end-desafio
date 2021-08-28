1120a26053c486ff5b9343fc743bd17f90d10f6b
devcamila <camilacerda3@gmail.com>
2021-08-28 11:41:48 -0300

    front-end05

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: `process.env.REACT_API_URL/cadastro`,
+    baseURL: 'https://back-end-desafio.herokuapp.com',
 })
 
 export default api;
